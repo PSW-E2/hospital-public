6e9d29069fed21bc6c33c9e3a24f2648acb54450
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-12-12 13:32:38 +0100

    update

--- src/app/modules/pages/home/model/schedules.model.ts
+++ src/app/modules/pages/home/model/schedules.model.ts
@@ -2,6 +2,6 @@ export class Appointment {
     appointmentId: number = 0;
     date: string = '';
     time: string = '';
-    patientId: number = 0;
-    doctorId: number = 0;
+    name: string = '';
+    surname: string = '';
 }
